2280315239042ac71c0226d1d7cb6492adf997b6
Rangel-Floripa <rangelflorianopolis@gmail.com>
2022-06-18 08:12:24 -0300

    vamos começar..

---
+++ algoritmo1.js
@@ -0,0 +1,4 @@
+let livros = [25, 15, 30, 50, 45, 20, 19, 11];
+livros.sort((a,b)=>a-b)
+console.log(`O livro mais barato custa ${livros[0]}
+`)
---
+++ index.js
@@ -0,0 +1,13 @@
+const precosLivros = [25, 15, 30, 50, 45, 20];
+
+
+let maisBarato = 0;
+
+for (let atual = 0; atual < precosLivros.length; atual++) {
+ if (precosLivros[atual] < precosLivros[maisBarato]) {
+     maisBarato = atual
+ }
+
+}
+
+console.log(`o livro mais barato custa ${precosLivros[maisBarato]}`)
